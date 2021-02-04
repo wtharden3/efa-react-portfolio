f827aa0efb70d8a75ef13d230f515e33d9bb2498
Whitney Harden <wtharden320@gmail.com>
2021-02-04 13:32:26 -0500

    starting menu/nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
+import {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.less';
-import { Button } from 'antd';
+import { Button, Menu } from 'antd';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Resume from './Components/Resume/Resume';
 
+const { SubMenu } = Menu;
+
 const App = () => {
+  
+  const [collapsed, setCollapsed] = useState(false);
+
+  useEffect(() => {
+    setCollapsed(!collapsed);
+  }, [collapsed])
+
   return (
     <div className="App">
       <header className="App-header">
+        <Menu>
+          <Button type="primary">Button</Button>
+          <Menu.Item key="1">First Menu Item</Menu.Item>
+          <Menu.Item key="2">Second Menu Item</Menu.Item>
+          <Menu.Item key="3">Third Menu Item</Menu.Item>
+          <SubMenu key="sub1" title="Forth Menu Item">
+            <Menu.ItemGroup>
+              <Menu.Item key="4">Option 1</Menu.Item>
+              <Menu.Item key="5">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+        </Menu>
         <Router>
           <div>
             <Link to="/">Home</Link>
